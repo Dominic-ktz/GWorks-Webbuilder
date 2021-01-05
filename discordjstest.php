0d028a5612a2b6e829974384392a250ea6d22b74
Dominic-ktz <dominickoppatz2003@gmail.com>
2021-01-05 15:19:51 +0100

    add discordjs

---
+++ discordjstest.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script src="https://cdn.g-works.eu/js/discord.js-webpack/discord.stable.min.js"></script>
+<script type="text/javascript">
+  const client = new Discord.Client();
+
+  client.on('message', msg => {
+    const guildTag = msg.channel.type === 'text' ? `[${msg.guild.name}]` : '[DM]';
+    const channelTag = msg.channel.type === 'text' ? `[#${msg.channel.name}]` : '';
+    console.log(`${guildTag}${channelTag} ${msg.author.tag}: ${msg.content}`);
+  });
+
+  client.login('NzM0MzU0NzI2OTg4MjgzOTA1.XxQfIg.eeDYQv12N1cp5GMLiAgQvIaPwyo');
+</script>
+</html>
